5be6c0e53768177213fe8e982c56c38602c5f716
babakfifoo <b.firoozi.f@gmail.com>
2026-02-18 20:37:16 +0200

    HTML string parsing is implemented.

--- app/services/DataProcessor.py
+++ app/services/DataProcessor.py
@@ -16,3 +16,24 @@ def Process_HTMLs():
 
         with open(dest_dir / (fp.stem + ".json"), mode="w") as f:
             f.write(json.dumps(data, ensure_ascii=False).replace("\t", ""))
+
+
+def Process_HTML_str(html_str):
+    """
+    Process a single HTML string and return the extracted data as a dictionary.
+    """
+    soup = BeautifulSoup(html_str, "html.parser")
+    data = DataExtractor.compile_record_json(soup)
+
+    return data
+
+
+# WARNING: process the data with GDPR before storing it!
+def Generate_tables(data):
+    # Implement table generation logic here
+    pass
+
+
+def process_ad_description(data):
+    # Implement data generation logic here
+    pass
